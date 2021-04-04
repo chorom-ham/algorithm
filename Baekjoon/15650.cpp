686ed79b30fab6db52d5e21bc6b0d6e89165ba65
Chorom Ham <chorom.m.ham@gmail.com>
2021-04-04 22:02:10 +0900

    Create 15650.cpp

---
+++ Baekjoon/15650.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+
+int n, m;
+int arr[9];
+
+void dfs(int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+		return;
+	}
+
+	for (int i = cnt == 0 ? 1 : arr[cnt - 1] + 1; i <= n; i++) {
+		arr[cnt] = i;
+		dfs(cnt + 1);
+	}
+}
+
+int main() {
+	scanf("%d %d", &n, &m);
+	dfs(0);
+}
